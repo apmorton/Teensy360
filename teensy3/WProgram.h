3ccb742fecaf1a45cefbf413d0d368a1ef51d247
Austin Morton <amorton@juvsoft.com>
2013-11-16 23:45:23 -0500

    Import teensy-template Remove non-used portions

---
+++ teensy3/WProgram.h
@@ -0,0 +1,27 @@
+#ifndef WProgram_h
+#define WProgram_h
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "wiring.h"
+#include "HardwareSerial.h"
+
+#define DMAMEM __attribute__ ((section(".dmabuffers"), used))
+
+#ifdef __cplusplus
+
+#include "usb_serial.h"
+#include "usb_seremu.h"
+
+#include "elapsedMillis.h"
+#include "IntervalTimer.h"
+
+unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
+
+#include "pins_arduino.h"
+
+#endif // __cplusplus
+
+#endif // WProgram_h
